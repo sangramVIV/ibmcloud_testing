c6900f4c64b3437fb8206b37c6cec0a623cb2b1e
SANGRAM BEHERA <60352246+sangramVIV@users.noreply.github.com>
2025-04-12 06:40:24 +0530

    Delete sumdocker directory

--- sumdocker/sum.py
+++
@@ -1,9 +0,0 @@
-
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-# Calculate the sum
-total = num1 + num2
-
-# Print the result
-print("The sum of the two numbers is:", total)
